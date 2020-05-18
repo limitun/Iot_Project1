2ac3814f1d388ec26edd8f7470c1481b6df5c857
limitun <juchan1213@naver.com>
2020-05-18 22:43:13 +0900

    nodejs commit

--- testfile.js
+++ testfile.js
@@ -1,1 +1,7 @@
-console.log('aaa');
+var http = require('http'); // node 내장 모듈 불러옴 
+var hostname = '127.0.0.1'; // localhost와 동일 
+var port = 3000; 
+http.createServer(function(req, res){ 
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    res.end('Hello World\n'); }).listen(port, hostname); 
+console.log('Server running at http://'+hostname+':'+port);
