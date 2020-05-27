f30c62975bcb4c361b77eee2384a34aca0ec6c22
limitun <juchan1213@naver.com>
2020-05-27 14:08:07 +0900

    git 0527 last commit

--- Web_TEST/lib/template2.js
+++ Web_TEST/lib/template2.js
@@ -9,21 +9,7 @@ module.exports = {
       <title>EMMas</title>
       <meta charset="utf-8">
     </head>
-    <body class="vbox">
-        <header><h1 class="type1"><a href="/manage/">EMMaS 기자재 정보 관리 시스템</a></h1></header>
-        <section class="main hbox space-between">
-          <article class="flex">flex</article>
-          <article class="flex">flex</article>
-        </section>
-        <section class="hbox space-between" style="height: 40%">
-          <article class="flex">flex</article>
-          <article class="flex">flex</article>
-        </section>
-        
-        ${body}
-        
-        <footer class="type1"><a href="/manage/logout">EMMaS 기자재 정보 관리 시스템</a></footer>
-    </body>
+    ${body}
     </html>
     `;
   },list:function(flist){
@@ -34,5 +20,34 @@ module.exports = {
       i=i+1;
     }list= list+'</ul>';
     return list;
+  },create_table:function(list){
+    var i=0;
+    var tmpt = `<table border="1" collapse:"true"><tr><th>기자재 번호</th><th>분류</th><th>등급</th>
+    <th>상태</th><th>제조사</th><th>장소</th><th>종류</th>,<th>기자재명</th>
+    <th>링크</th>`;
+    while(i<list.length){
+      tmpt=tmpt+'<tr>'
+      var stat='';  //상태 관리 조건문
+      var tmp1=list[i]['eq_status'];
+      if(tmp1=='in_use'){
+        stat='in_use';
+      }else if(tmp1=='available'){
+        stat='avail';
+      }else if(tmp1=='unavail'){
+        stat='x';
+      }else{
+        stat='o';
+      }
+      for(var key in list[i]){
+        if(key=='note'||key=='acquisition'){
+          continue;
+        }else{
+          tmpt=tmpt+`<td class="${stat}">${list[i][key]}</td>`;
+        }
+        
+      }tmpt+=`<td><a href="/">상세정보</a></td></tr>`;//수정 하이퍼링크
+      i=i+1;
+    }tmpt= tmpt+'</table>';
+    return tmpt;
   }
 }
--- Web_TEST/routes/manage.js
+++ Web_TEST/routes/manage.js
@@ -24,7 +24,18 @@ module.exports = function (passport) {
   router.get('/', function (request, response) {
     var title = 'manage';
     var list = '';
-    var html = template.HTML(title, '',
+    var html = template.HTML(title, `<body class="vbox">
+    <header><h1 class="type1"><a href="/manage/">EMMaS 기자재 정보 관리 시스템</a></h1></header>
+    <section class="main hbox space-between">
+      <article class="flex"><a href="/manage/table">기자재 조회/사용</a></article>
+      <article class="flex"><a href="/manage/">설정관리</a></article>
+    <!--</section>-->
+    <!--<section class="hbox space-between" style="height: 40%">-->
+    </section>
+    
+    <footer class="type1"><a href="/manage/table"><br>EMMaS 기자재 정보 관리 시스템</a></footer>
+    <footer class="type1"><a href="/manage/logout">로그아웃</a></footer>
+</body>`,
       '');
       response.send(html);
   });
@@ -35,13 +46,18 @@ module.exports = function (passport) {
     connection.query('SELECT * from equipment ', function(error,results,fields){
       if(error){
         console.log(error);
-        var html = template.HTML(title, error,
+        var html = template.HTML(title, `<body class="vbox">
+        <header><h1 class="type1"><a href="/manage/">EMMaS 기자재 정보 관리 시스템</a></h1></header>
+        ${error}<br><body>`,
           '');
         response.send(html);
       }else{
-        list = template.list(results);    
-        console.log(list);
-        var html = template.HTML(title, list,
+        list = template.create_table(results);   
+        console.log(results);
+        var html = template.HTML(title, `<body class="vbox">
+        <header><h1 class="type1"><a href="/manage/">EMMaS 기자재 정보 관리 시스템</a></h1></header>
+        ${list}<footer class="type1"><a href="/manage/table">EMMaS 기자재 정보 관리 시스템</a></footer>
+        <footer class="type1"><a href="/manage/logout">로그아웃</a></footer></body>`,
         '');
         response.send(html);
       }
